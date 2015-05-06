1517b17d7843300eeb7c802f88f6520d07845f71
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-05-06 10:14:40 -0400

    add chown to setup

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ else:
     user = os.environ['USER']
 
 home = getpwnam(user).pw_dir
+uid  = getpwnam(user).pw_uid
+gid  = getpwnam(user).pw_gid
 
 config_dir = home + '/.tagshell'
 config_file = home + '/.tagshell/config.yaml'
@@ -19,12 +21,15 @@ tag_file = home + '/.tagshell/tags.yaml'
 
 if not os.path.isdir(config_dir):
     os.mkdir(config_dir)
+    os.chown(config_dir,uid,gid)
 
 if not os.path.isfile(config_file):
     shutil.copyfile('config_example.yaml', config_file)
+    os.chown(config_dir,uid,gid)
 
 if not os.path.isfile(tag_file):
     shutil.copyfile('tags_example.yaml', tag_file)
+    os.chown(config_dir,uid,gid)
 
 setup(name='tagshell',
         version=__version__,
